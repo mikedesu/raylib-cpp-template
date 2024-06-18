453d73708e19b539bf5f894a57b7b118d44abb8b
darkmage <165251+mikedesu@users.noreply.github.com>
2024-06-18 02:57:41 -0500

    update

--- main.cpp
+++ main.cpp
@@ -1,68 +1,93 @@
 #include "raylib.h"
+// #include "rcamera.h"
 
-RenderTexture target;
-Rectangle screenRect;
-// Camera2D camera = {0};
-Camera3D camera = {0};
-CameraMode camera_mode = CAMERA_CUSTOM;
+#include <string>
+
+using std::string;
+using std::to_string;
+
+void draw_debug_panel(Camera2D &camera, Font &font);
 
 int main(void) {
   const int screenWidth = 1280;
   const int screenHeight = 720;
-
   const char *title = "test";
+  bool debug_panel_on = true;
+  RenderTexture target;
+  Rectangle screenRect;
+  Camera2D camera = {0};
+  const char *img_path = "../ecs/img/skull-sheet.png";
+  float scale = 8;
+  Rectangle skull_rect;
+  Vector2 skull_position = (Vector2){0, 0};
+  const char *font_path = "hack.ttf";
+  Font font;
+  int base_move_unit = 1;
+  int move_unit = base_move_unit * scale;
 
   InitWindow(screenWidth, screenHeight, title);
   target = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
-  screenRect = (Rectangle){0, 0, -screenWidth, screenHeight};
-
-  // 2d camera
-  // camera.target = (Vector2){0, 0};
-  // camera.offset = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
-  // camera.rotation = 0.0f;
-  // camera.zoom = 1.0f;
-
-  camera.position = (Vector3){10.0f, 10.0f, 10.0f};
-  camera.target = (Vector3){0.0f, 0.0f, 0.0f};
-  camera.up = (Vector3){0.0f, 1.0f, 0.0f};
-  camera.fovy = 45.0f;
-
+  screenRect = (Rectangle){0, 0, screenWidth, -screenHeight};
   // set the button to close the window
   SetExitKey(KEY_Q);
 
+  Texture2D texture = LoadTexture(img_path);
+  skull_rect =
+      (Rectangle){0, 0, (float)texture.width / 2.0f, (float)texture.height};
+  font = LoadFont(font_path);
+  // DisableCursor();
+
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
   while (!WindowShouldClose()) {
+    if (IsKeyDown(KEY_UP)) {
+      skull_position.y -= move_unit;
+    } else if (IsKeyDown(KEY_DOWN)) {
+      skull_position.y += move_unit;
+    }
 
-    // if (IsKeyDown(KEY_LEFT)) {
-    //   camera.target.x -= 10;
-    // } else if (IsKeyDown(KEY_RIGHT)) {
-    //   camera.target.x += 10;
-    // } else if (IsKeyDown(KEY_UP)) {
-    //   camera.target.y -= 10;
-    // } else if (IsKeyDown(KEY_DOWN)) {
-    //   camera.target.y += 10;
-    // }
+    if (IsKeyDown(KEY_LEFT)) {
+      skull_position.x -= move_unit;
+    } else if (IsKeyDown(KEY_RIGHT)) {
+      skull_position.x += move_unit;
+    }
+
+    if (IsKeyPressed(KEY_F)) {
+      ToggleFullscreen();
+    }
+
+    if (IsKeyPressed(KEY_D)) {
+      debug_panel_on = !debug_panel_on;
+    }
 
     BeginDrawing();
+    BeginMode2D(camera);
     BeginTextureMode(target);
-    // BeginMode2D(camera);
-    // ClearBackground(BLACK);
-    // DrawRectangle(0, 0, 100, 100, RED);
-    // EndMode2D();
-
-    BeginMode3D(camera);
-
-    ClearBackground(RAYWHITE);
-    DrawGrid(10, 1.0f);
-    DrawCube((Vector3){0, 0, 0}, 2.0f, 2.0f, 2.0f, RED);
-    EndMode3D();
-
+    ClearBackground(WHITE);
+    DrawTexturePro(texture, skull_rect,
+                   (Rectangle){skull_position.x, skull_position.y,
+                               ((float)texture.width / 2) * scale,
+                               texture.height * scale},
+                   (Vector2){0, 0}, 0, WHITE);
+    // DrawRectangleLines(48, 24, 24, 24, RED);
     EndTextureMode();
+    EndMode2D();
     DrawTextureRec(target.texture, screenRect, (Vector2){0, 0}, WHITE);
     DrawFPS(10, 10);
+
+    // draw debug panel
+    if (debug_panel_on) {
+      draw_debug_panel(camera, font);
+    }
     EndDrawing();
   }
   UnloadRenderTexture(target);
   CloseWindow();
   return 0;
+}
+
+void draw_debug_panel(Camera2D &camera, Font &font) {
+  string camera_info_str = "Camera: " + to_string(camera.target.x) + ", " +
+                           to_string(camera.target.y);
+  DrawRectangle(0, 0, 500, 200, Fade(BLACK, 0.5f));
+  DrawTextEx(font, camera_info_str.c_str(), (Vector2){10, 10}, 16, 0.5f, WHITE);
 }
